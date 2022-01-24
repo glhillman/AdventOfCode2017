0bc9f54622878b7b9b01d2348eca7e143155b610
glhillman <glhillman@gmail.com>
2022-01-24 12:46:37 -0700

    Day01

---
+++ Day01/DayClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01
+{
+    internal class DayClass
+    {
+        public string _input;
+        public DayClass()
+        {
+            LoadData();
+        }
+
+        public void Part1()
+        {
+            int sum = 0;
+            for (int i = 0; i < _input.Length; i++)
+            {
+                if (_input[i] == _input[(i+1) % _input.Length])
+                {
+                    sum += int.Parse(_input[i].ToString());
+                }
+            }
+
+            Console.WriteLine("Part1: {0}", sum);
+        }
+
+        public void Part2()
+        {
+            int sum = 0;
+            int bias = _input.Length / 2;
+            for (int i = 0; i < _input.Length; i++)
+            {
+                if (_input[i] == _input[(i + bias) % _input.Length])
+                {
+                    sum += int.Parse(_input[i].ToString());
+                }
+            }
+
+            Console.WriteLine("Part1: {0}", sum);
+        }
+
+        private void LoadData()
+        {
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                StreamReader file = new StreamReader(inputFile);
+                _input = file.ReadLine();
+                file.Close();
+            }
+        }
+
+    }
+}
