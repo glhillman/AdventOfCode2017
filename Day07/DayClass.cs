a2f9796585dac5be17c1e7379afcef8be9848b57
glhillman <glhillman@gmail.com>
2022-01-27 12:53:11 -0700

    Day07

---
+++ Day07/DayClass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    internal class DayClass
+    {
+        List<Node> _nodes = new List<Node>();
+        public DayClass()
+        {
+            LoadData();
+            _nodes.Sort(delegate (Node x, Node y) { return x.Weight - y.Weight;});
+        }
+
+        public void Part1()
+        {
+            Node root = _nodes.First(n => n.Parent == null);
+
+            Console.WriteLine("Part1: {0}", root.Name);
+        }
+
+        public void Part2()
+        {
+            Node node = _nodes.First(n => n.Parent == null);
+            int targetWeight = 0;
+
+            // follow the unbalanced node until it is balanced
+            while (!node.IsBalanced)
+            {
+                (node, targetWeight) = node.GetUnbalancedChild();
+            }
+            
+            int adjustedWeight = node.Weight - (node.TotalWeight - targetWeight);
+
+            Console.WriteLine("Part2: {0}", adjustedWeight);
+        }
+        private void LoadData()
+        {
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                string? line;
+                StreamReader file = new StreamReader(inputFile);
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] parts = line.Split("->");
+                    // process left side
+                    string[] parts2 = parts[0].Split(new char[] {' ', '(', ')'});
+                    string name = parts2[0].Trim();
+                    Node? parent = _nodes.FirstOrDefault(n => n.Name == name);
+                    if (parent == null)
+                    {
+                        parent = new Node(name);
+                        _nodes.Add(parent);
+                    }
+                    parent.Weight = int.Parse(parts2[2]);
+
+                    // process right side if any
+                    if (parts.Length > 1)
+                    {
+                        parts2 = parts[1].Split(',');
+                        foreach (string part in parts2)
+                        {
+                            name = part.Trim();
+                            Node? child = _nodes.FirstOrDefault((n) => n.Name == name);
+                            if (child == null)
+                            {
+                                child = new Node(name);
+                                _nodes.Add(child);
+                            }
+                            child.Parent = parent;
+                            parent.Children.Add(child);
+                        }
+                    }
+                }
+
+                file.Close();
+            }
+        }
+
+    }
+}
---
+++ Day07/Node.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    internal class Node
+    {
+        public Node(string name)
+        {
+            Name = name;
+            Children = new List<Node>();
+            Parent = null;
+        }
+        public int Weight { get; set; }
+        public string Name { get; private set; }
+        public Node? Parent { get; set; }
+        public List<Node> Children { get; set;}
+        public int ChildrenWeight { get { return Children.Sum(c => c.TotalWeight); } }
+        public int TotalWeight { get { return Weight + ChildrenWeight; } }
+        public bool IsBalanced
+        {
+            get
+            {
+                var groups = Children.GroupBy(x => x.TotalWeight);
+                return groups.Count() == 1;
+            }
+        }
+        public (Node node, int targetWeight) GetUnbalancedChild()
+        {
+            // we know coming in that this is an unbalanced group
+            // group by totalweight
+            var groups = Children.GroupBy(x => x.TotalWeight);
+            // get the weight value of majority group (there will be only one or two groups)
+            var targetWeight = groups.First(x => x.Count() > 1).Key;
+            // get the group with only one entry (this is the unbalanced one)
+            var unbalancedChild = groups.First(x => x.Count() == 1).First();
+
+            // return the unbalanced node with the expected weight value
+            return (unbalancedChild, targetWeight);
+        }
+        public override string ToString()
+        {
+            string tostring = String.Format("Name: {0}, Weight: {1}, Parent: {2}, N Children: {3}, Children Weight: {4}, Total Weight: {5}",
+                                             Name, Weight, Parent == null ? "NULL" : Parent.Name, Children.Count, ChildrenWeight, TotalWeight);
+            if (Children.Count > 0)
+            {
+                tostring += ", IsBalanced: " + IsBalanced.ToString();
+            }
+            return tostring;
+        }
+    }
+}
