6f6ed4adbb520751ff1a381831ecd01fa52b9b33
glhillman <glhillman@gmail.com>
2022-01-29 18:12:00 -0700

    Day13

---
+++ Day13/DayClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13
+{
+    internal class DayClass
+    {
+        List<Layer> _layers = new();
+        public DayClass()
+        {
+            LoadData();
+        }
+
+        public void Part1()
+        {
+            int severity = 0;
+            foreach (Layer layer in _layers)
+            {
+                if (layer.IsCaught(0))
+                {
+                    severity += layer.Depth * layer.Range;
+                }
+            }
+
+            Console.WriteLine("Part1: {0}", severity);
+        }
+
+        public void Part2()
+        {
+            int step = 0;
+            bool caught;
+            do
+            {
+                step++;
+                caught = false;
+
+                for (int i = 0; !caught && i < _layers.Count; i++)
+                {
+                    if (_layers[i].IsCaught(step))
+                    {
+                        caught = true;
+                        break;
+                    }
+                }
+            } while (caught);
+
+            Console.WriteLine("Part2: {0}", step);
+        }
+
+        private void LoadData()
+        {
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                string? line;
+                StreamReader file = new StreamReader(inputFile);
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(':');
+                    int depth = int.Parse(parts[0]);
+                    int range = int.Parse(parts[1]);
+                    _layers.Add(new Layer(depth, range));
+                }
+
+                file.Close();
+            }
+        }
+
+    }
+}
---
+++ Day13/Layer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13
+{
+    internal class Layer
+    {
+        public Layer(int depth, int range)
+        {
+            Depth = depth;
+            Range = range;
+        }
+
+        public int Depth { get; private set; }
+        public int Range { get; private set; }
+        public bool IsCaught(int step)
+        {
+            return (step + Depth)  % ((Range - 1) * 2) == 0;
+        }
+        public override string ToString()
+        {
+            return String.Format("Depth: {0}, Range: {1}", Depth, Range);
+        }
+    }
+}
