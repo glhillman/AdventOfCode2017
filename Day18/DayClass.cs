4b4c6635f51530a26a4250d77b9cc12523b22e1b
glhillman <glhillman@gmail.com>
2022-02-07 19:57:11 -0700

    Day18

---
+++ Day18/DayClass.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18
+{
+    internal class DayClass
+    {
+        private List<Instruction> _instructions = new();
+        private Dictionary<char, long> _vars = new();
+        public DayClass()
+        {
+            LoadData();
+        }
+
+        public void Part1()
+        {
+            long lastSound = RunInstructions(_instructions, _vars);
+
+            Console.WriteLine("Part1: {0}", lastSound);
+        }
+
+        public void Part2()
+        {
+            Queue<long> q0 = new Queue<long>();
+            Queue<long> q1 = new Queue<long>();
+            foreach (char c in _vars.Keys)
+            {
+                _vars[c] = 0;
+            }
+            Dictionary<char, long> vars2 = new Dictionary<char, long>(_vars);
+            vars2['p'] = 1;
+            Machine m0 = new Machine(_instructions, _vars, q0, q1);
+            Machine m1 = new Machine(_instructions, vars2, q1, q0);
+            bool stillRunning;
+            do
+            {
+                stillRunning = m0.Process();
+                stillRunning = stillRunning || m1.Process();
+            } while (stillRunning);
+
+            Console.WriteLine("Part2: {0}", m1.NWrites);
+        }
+
+        private long RunInstructions(List<Instruction> instructions, Dictionary<char, long> vars)
+        {
+            long lastSound = 0;
+            long maxIndex = instructions.Count;
+            long index = 0;
+
+            while (index >= 0 && index < maxIndex)
+            {
+                Instruction inst = instructions[(int)index];
+                switch (inst.InstType)
+                {
+                    case InstructionType.snd:
+                        lastSound = vars[inst.XVar];
+                        index++;
+                        break;
+                    case InstructionType.set:
+                        vars[inst.XVar] = inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                        index++;
+                        break;
+                    case InstructionType.add:
+                        vars[inst.XVar] += inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                        index++;
+                        break;
+                    case InstructionType.mul:
+                        vars[inst.XVar] *= inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                        index++;
+                        break;
+                    case InstructionType.mod:
+                        vars[inst.XVar] %= inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                        index++;
+                        break;
+                    case InstructionType.rcv:
+                        if (vars[inst.XVar] != 0)
+                        {
+                            vars[inst.XVar] = lastSound;
+                            return lastSound;
+                        }
+                        index++;
+                        break;
+                    case InstructionType.jgz:
+                        if (inst.XIsValue)
+                        {
+                            if (inst.XValue > 0)
+                            {
+                                index += inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                            }
+                            else
+                            {
+                                index++;
+                            }
+                        }
+                        else if (vars[inst.XVar] > 0)
+                        {
+                            index += inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                        }
+                        else
+                        {
+                            index++;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return lastSound;
+        }
+
+        private void LoadData()
+        {
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                string? line;
+                StreamReader file = new StreamReader(inputFile);
+                Instruction? inst = null;
+                while ((line = file.ReadLine()) != null)
+                {
+                    bool xIsValue = false;
+                    bool yIsValue = false;
+                    char xVar = ' ';
+                    long xValue = 0;
+                    char yVar = ' ';
+                    long yValue = 0;
+
+                    string[] parts = line.Split(' ');
+                    switch (parts[0])
+                    {
+                        case "snd":
+                            xVar = parts[1][0];
+                            _vars[xVar] = 0;
+                            inst = new Instruction(InstructionType.snd, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                        case "set":
+                            xVar = parts[1][0];
+                            _vars[xVar] = 0;
+                            yIsValue = long.TryParse(parts[2], out yValue);
+                            if (yIsValue == false)
+                            {
+                                yVar = parts[2][0];
+                                _vars[yVar] = 0;
+                            }
+                            inst = new Instruction(InstructionType.set, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                        case "add":
+                            xVar = parts[1][0];
+                            _vars[xVar] = 0;
+                            yIsValue = long.TryParse(parts[2], out yValue);
+                            if (yIsValue == false)
+                            {
+                                yVar = parts[2][0];
+                                _vars[yVar] = 0;
+                            }
+                            inst = new Instruction(InstructionType.add, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                        case "mul":
+                            xVar = parts[1][0];
+                            _vars[xVar] = 0;
+                            yIsValue = long.TryParse(parts[2], out yValue);
+                            if (yIsValue == false)
+                            {
+                                yVar = parts[2][0];
+                                _vars[yVar] = 0;
+                            }
+                            inst = new Instruction(InstructionType.mul, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                        case "mod":
+                            xVar = parts[1][0];
+                            _vars[xVar] = 0;
+                            yIsValue = long.TryParse(parts[2], out yValue);
+                            if (yIsValue == false)
+                            {
+                                yVar = parts[2][0];
+                                _vars[yVar] = 0;
+                            }
+                            inst = new Instruction(InstructionType.mod, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                        case "rcv":
+                            xVar = parts[1][0];
+                            _vars[xVar] = 0;
+                            inst = new Instruction(InstructionType.rcv, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                        case "jgz":
+                            xIsValue = long.TryParse(parts[1], out xValue);
+                            if (xIsValue == false)
+                            {
+                                xVar = parts[1][0];
+                                _vars[xVar] = 0;
+                            }
+                            yIsValue = long.TryParse(parts[2], out yValue);
+                            if (yIsValue == false)
+                            {
+                                yVar = parts[2][0];
+                                _vars[yVar] = 0;
+                            }
+                            inst = new Instruction(InstructionType.jgz, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                    }
+
+                    _instructions.Add(inst);
+                }
+
+                file.Close();
+            }
+        }
+
+    }
+}
---
+++ Day18/Instruction.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18
+{
+    enum InstructionType
+    {
+        snd,
+        set,
+        add,
+        mul,
+        mod,
+        rcv,
+        jgz
+    };
+
+    internal class Instruction
+    {
+        public Instruction(InstructionType instType, bool xIsValue, char xVar, long xValue, bool yIsValue, char yVar, long yValue)
+        {
+            InstType = instType;
+            XIsValue = xIsValue;
+            XVar = xVar;
+            XValue = xValue;
+            YIsValue = yIsValue;
+            YVar = yVar;
+            YValue = yValue;
+        }
+        public InstructionType InstType { get; private set; }
+        public bool XIsValue { get; private set; }
+        public char XVar { get; private set; }
+        public long XValue { get; private set; }
+        public bool YIsValue { get; private set; }
+        public char YVar { get; private set; }
+        public long YValue { get; private set; }
+        public override string ToString()
+        {
+            string toString = InstType.ToString() + " " + XVar.ToString() + " ";
+            if (!(InstType == InstructionType.snd || InstType == InstructionType.rcv))
+            {
+                toString += YIsValue ? YValue : YVar.ToString();
+            }
+            return toString;
+        }
+    }
+
+    internal class Machine
+    {
+        public Machine(List<Instruction> instructions, Dictionary<char, long> vars, Queue<long> inputQ, Queue<long> outputQ)
+        {
+            Instructions = instructions;
+            Vars = vars;
+            InputQ = inputQ;
+            OutputQ = outputQ;
+            Index = 0;
+            NWrites = 0;
+            MaxIndex = Instructions.Count;
+        }
+
+        public List<Instruction> Instructions { get; private set; }
+        public Dictionary<char, long> Vars { get; private set; }
+        public long Index { get; set; }
+        public long NWrites { get; set; }
+        public Queue<long> InputQ { get; private set; }
+        public Queue<long> OutputQ { get; private set; }
+        private long MaxIndex { get; set; }
+        public bool Process()
+        {
+            int nReads = 0;
+            int nWrites = 0;
+            while (Index >= 0 && Index < MaxIndex)
+            {
+                Instruction inst = Instructions[(int)Index];
+                switch (inst.InstType)
+                {
+                    case InstructionType.snd:
+                        OutputQ.Enqueue(Vars[inst.XVar]);
+                        NWrites++;
+                        nWrites++;
+                        nReads++;
+                        Index++;
+                        break;
+                    case InstructionType.set:
+                        Vars[inst.XVar] = inst.YIsValue ? inst.YValue : Vars[inst.YVar];
+                        Index++;
+                        break;
+                    case InstructionType.add:
+                        Vars[inst.XVar] += inst.YIsValue ? inst.YValue : Vars[inst.YVar];
+                        Index++;
+                        break;
+                    case InstructionType.mul:
+                        Vars[inst.XVar] *= inst.YIsValue ? inst.YValue : Vars[inst.YVar];
+                        Index++;
+                        break;
+                    case InstructionType.mod:
+                        Vars[inst.XVar] %= inst.YIsValue ? inst.YValue : Vars[inst.YVar];
+                        Index++;
+                        break;
+                    case InstructionType.rcv:
+                        if (InputQ.Count > 0)
+                        {
+                            Vars[inst.XVar] = InputQ.Dequeue();
+                            Index++;
+                        }
+                        else
+                        {
+                            return nWrites > 0 || nReads > 0;
+                        }
+                        break;
+                    case InstructionType.jgz:
+                        if (inst.XIsValue)
+                        {
+                            if (inst.XValue > 0)
+                            {
+                                Index += inst.YIsValue ? inst.YValue : Vars[inst.YVar];
+                            }
+                            else
+                            {
+                                Index++;
+                            }
+                        }
+                        else if (Vars[inst.XVar] > 0)
+                        {
+                            Index += inst.YIsValue ? inst.YValue : Vars[inst.YVar];
+                        }
+                        else
+                        {
+                            Index++;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return false;
+        }
+    }
+}
