accf7e66fedc84ad52a245d0a8c9859f2ceadb40
glhillman <glhillman@gmail.com>
2022-02-09 09:28:42 -0700

    Day20

---
+++ Day20/DayClass.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20
+{
+    internal class DayClass
+    {
+        public DayClass()
+        {
+            LoadData();
+        }
+
+        public void Part1()
+        {
+            List<Particle> particles = LoadData();
+
+            List<int> nearest = new();
+
+            for (int i = 0; i < 500; i++)
+            {
+                foreach (Particle p in particles)
+                {
+                    p.DoTick();
+                }
+                particles.Sort((a, b) => a.Manhattan < b.Manhattan ? -1 : 1);
+                nearest.Add(particles[0].Id);
+            }
+            var rslts = from manhattan in nearest
+                        group manhattan by manhattan into grp
+                        let count = grp.Count()
+                        orderby count descending
+                        select new { Value = grp.Key, Count = count };
+
+            var rslt = rslts.First();
+
+            Console.WriteLine("Part1: {0}", rslt.Value);
+        }
+
+        public void Part2()
+        {
+            List<Particle> particles = LoadData();
+            int particleCount = particles.Count();
+
+            for (int i = 0; i < 50; i++)
+            {
+                foreach (Particle p in particles)
+                {
+                    p.DoTick();
+                }
+                // eliminate collisions
+                for (int p = 0; p < particleCount - 1; p++)
+                {
+                    if (particles[p].IsDuplicate == false)
+                    {
+                        for (int j = p + 1; j < particleCount; j++)
+                        {
+                            if (particles[j].IsDuplicate == false)
+                            {
+                                if (particles[p].IsEqualTo(particles[j]))
+                                {
+                                    particles[p].IsDuplicate = true;
+                                    particles[j].IsDuplicate = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            int rslt = particles.Count(p => p.IsDuplicate == false);
+
+            Console.WriteLine("Part1: {0}", rslt);
+        }
+
+        private List<Particle> LoadData()
+        {
+            List<Particle> particles = new();
+
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                char[] delimiters = new char[] { '<', ',', '>' };
+                int id = 0;
+                string? line;
+                StreamReader file = new StreamReader(inputFile);
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(delimiters);
+                    particles.Add(new Particle(id++, PartsToVector(parts, 1), PartsToVector(parts, 6), PartsToVector(parts, 11)));
+                }
+
+                file.Close();
+            }
+
+            return particles;
+        }
+
+        private Vector PartsToVector(string[] parts, int startIndex)
+        {
+            long x = long.Parse(parts[startIndex++]);
+            long y = long.Parse(parts[startIndex++]);
+            long z = long.Parse(parts[startIndex++]);
+
+            return new Vector(x, y, z);
+        }
+
+    }
+}
---
+++ Day20/Particle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20
+{
+    internal class Vector
+    {
+        public Vector (long x, long y, long z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public long X { get; set; }
+        public long Y { get; set; }
+        public long Z { get; set; }
+        public bool IsEqualTo(Vector other)
+        {
+            return X == other.X && Y == other.Y && Z == other.Z;
+        }
+        public override string ToString()
+        {
+            return String.Format("<{0},{1},{2}>", X, Y, Z);
+        }
+    }
+    internal class Particle
+    {
+        public Particle(int id, Vector position, Vector velocity, Vector acceleration)
+        {
+            Id = id;
+            Position = position;
+            Velocity = velocity;
+            Acceleration = acceleration;
+            Manhattan = Math.Abs(Velocity.X) + Math.Abs(Velocity.Y) + Math.Abs(Velocity.Z);
+            IsDuplicate = false;
+        }
+
+        public int Id { get; private set; }
+        public Vector Position { get; set; }
+        public Vector Velocity { get; set; }
+        public Vector Acceleration { get; set; }
+        public long Manhattan { get; set; }
+        public long DoTick()
+        {
+            Velocity.X += Acceleration.X;
+            Velocity.Y += Acceleration.Y;
+            Velocity.Z += Acceleration.Z;
+            Position.X += Velocity.X;
+            Position.Y += Velocity.Y;
+            Position.Z += Velocity.Z;
+            Manhattan = Math.Abs(Velocity.X) + Math.Abs(Velocity.Y) + Math.Abs(Velocity.Z);
+            return Manhattan;
+        }
+        public bool IsEqualTo(Particle other)
+        {
+            return Position.IsEqualTo(other.Position);
+        }
+        public bool IsDuplicate { get; set; }
+        public override string ToString()
+        {
+            return String.Format("Id: {0}, p={1}, v={2}>, a={3}, Man: {4}", Id, Position, Velocity, Acceleration, Manhattan);
+        }
+    }
+}
