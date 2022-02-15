c264fad66feb20e0a61f7ac134fccd8d00dab49f
glhillman <glhillman@gmail.com>
2022-02-15 07:02:54 -0700

    Day23

---
+++ Day23/DayClass.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23
+{
+    internal class DayClass
+    {
+        private List<Instruction> _instructions = new();
+        private Dictionary<char, long> _vars = new();
+
+        public DayClass()
+        {
+            LoadData();
+        }
+
+        public void Part1()
+        {
+            long nMuls = Process(_instructions, new Dictionary<char, long>(_vars));
+
+            Console.WriteLine("Part1: {0}", nMuls);
+        }
+
+        public void Part2()
+        {
+            //Dictionary<char, long> vars = new Dictionary<char, long>(_vars);
+            //vars['a'] = 1;
+            //Process(_instructions, vars);
+
+            // looking for non-primes between 107900 - 124900 step 17
+            long b = 0;
+            long c = 0;
+            long d = 0;
+            long e = 0;
+            long f = 0;
+            long g = 0;
+            long h = 0;
+
+            b = 79;
+            c = b;
+            b *= 100;
+            b += 100000;
+            c = b;
+            c += 17000;
+
+            while (b <= c)
+            {
+                for (d = 2; d != b; d++)
+                {
+                    if (b % d == 0)
+                    {
+                        h++;
+                        break;
+                    }
+                }
+                b += 17;
+            }
+
+            Console.WriteLine("Part2: {0}", h);
+        }
+
+        public long Process(List<Instruction> instructions, Dictionary<char, long> vars)
+        {
+            long index = 0;
+            long maxIndex = instructions.Count;
+            long nMul = 0;
+            while (index >= 0 && index < maxIndex)
+            {
+                Instruction inst = instructions[(int)index];
+                switch (inst.InstType)
+                {
+                    case InstructionType.set:
+                        vars[inst.XVar] = inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                        index++;
+                        break;
+                    case InstructionType.sub:
+                        vars[inst.XVar] -= inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                        if (inst.XVar == 'h')
+                        {
+                            Console.WriteLine("h = " + vars['h']);
+                        }
+                        index++;
+                        break;
+                    case InstructionType.mul:
+                        vars[inst.XVar] *= inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                        index++;
+                        nMul++;
+                        break;
+                    case InstructionType.jnz:
+                        if (inst.XIsValue)
+                        {
+                            if (inst.XValue != 0)
+                            {
+                                index += inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                            }
+                            else
+                            {
+                                index++;
+                            }
+                        }
+                        else if (vars[inst.XVar] != 0)
+                        {
+                            index += inst.YIsValue ? inst.YValue : vars[inst.YVar];
+                        }
+                        else
+                        {
+                            index++;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return nMul;
+        }
+
+        private void LoadData()
+        {
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                string? line;
+                StreamReader file = new StreamReader(inputFile);
+                Instruction? inst = null;
+                while ((line = file.ReadLine()) != null)
+                {
+                    bool xIsValue = false;
+                    bool yIsValue = false;
+                    char xVar = ' ';
+                    long xValue = 0;
+                    char yVar = ' ';
+                    long yValue = 0;
+
+                    string[] parts = line.Split(' ');
+                    switch (parts[0])
+                    {
+                        case "set":
+                            xVar = parts[1][0];
+                            _vars[xVar] = 0;
+                            yIsValue = long.TryParse(parts[2], out yValue);
+                            if (yIsValue == false)
+                            {
+                                yVar = parts[2][0];
+                                _vars[yVar] = 0;
+                            }
+                            inst = new Instruction(InstructionType.set, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                        case "sub":
+                            xVar = parts[1][0];
+                            _vars[xVar] = 0;
+                            yIsValue = long.TryParse(parts[2], out yValue);
+                            if (yIsValue == false)
+                            {
+                                yVar = parts[2][0];
+                                _vars[yVar] = 0;
+                            }
+                            inst = new Instruction(InstructionType.sub, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                        case "mul":
+                            xVar = parts[1][0];
+                            _vars[xVar] = 0;
+                            yIsValue = long.TryParse(parts[2], out yValue);
+                            if (yIsValue == false)
+                            {
+                                yVar = parts[2][0];
+                                _vars[yVar] = 0;
+                            }
+                            inst = new Instruction(InstructionType.mul, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                        case "jnz":
+                            xIsValue = long.TryParse(parts[1], out xValue);
+                            if (xIsValue == false)
+                            {
+                                xVar = parts[1][0];
+                                _vars[xVar] = 0;
+                            }
+                            yIsValue = long.TryParse(parts[2], out yValue);
+                            if (yIsValue == false)
+                            {
+                                yVar = parts[2][0];
+                                _vars[yVar] = 0;
+                            }
+                            inst = new Instruction(InstructionType.jnz, xIsValue, xVar, xValue, yIsValue, yVar, yValue);
+                            break;
+                    }
+
+                    _instructions.Add(inst);
+                }
+
+                file.Close();
+            }
+        }
+    }
+}
---
+++ Day23/Instruction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23
+{
+    enum InstructionType
+    {
+        set,
+        sub,
+        mul,
+        jnz
+    };
+
+    internal class Instruction
+    {
+        public Instruction(InstructionType instType, bool xIsValue, char xVar, long xValue, bool yIsValue, char yVar, long yValue)
+        {
+            InstType = instType;
+            XIsValue = xIsValue;
+            XVar = xVar;
+            XValue = xValue;
+            YIsValue = yIsValue;
+            YVar = yVar;
+            YValue = yValue;
+        }
+        public InstructionType InstType { get; private set; }
+        public bool XIsValue { get; private set; }
+        public char XVar { get; private set; }
+        public long XValue { get; private set; }
+        public bool YIsValue { get; private set; }
+        public char YVar { get; private set; }
+        public long YValue { get; private set; }
+        public override string ToString()
+        {
+            return InstType.ToString() + " " + XVar.ToString() + " " + (YIsValue ? YValue : YVar.ToString());
+        }
+    }
+}
